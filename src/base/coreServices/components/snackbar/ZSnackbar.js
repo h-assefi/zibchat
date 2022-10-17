f3710c04793291a2f57b152a7af40000eda71405
h-assefi <assefi.hossein@gmail.com>
2022-10-17 09:57:16 +0330

    ZSnackbar component continued

--- src/base/coreServices/components/snackbar/ZSnackbar.js
+++ src/base/coreServices/components/snackbar/ZSnackbar.js
@@ -2,40 +2,61 @@ import * as React from "react";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 
-export const Severity = {
+export const ZSnackbarSeverity = {
   error: "error",
   success: "success",
   warning: "warning",
   info: "info",
 };
 
-const Alert = React.forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
-});
+export const ZSnackbarPosition = {
+  topLeft: { vertical: "top", horizontal: "left" },
+  topCenter: { vertical: "top", horizontal: "center" },
+  topRight: { vertical: "top", horizontal: "right" },
+  bottomLeft: { vertical: "bottom", horizontal: "left" },
+  bottomCenter: { vertical: "bottom", horizontal: "center" },
+  bottomRight: { vertical: "bottom", horizontal: "right" },
+};
 
 const ZSnackbar = ({
   action,
   alert,
+  alertVariant,
   duration = 5000,
   message,
   onClose,
   open,
+  position,
   severity,
+  sx,
 }) => {
-  if (alert) {
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return (
+      <MuiAlert
+        elevation={6}
+        ref={ref}
+        variant={alertVariant ?? "standard"}
+        {...props}
+      />
+    );
+  });
+
+  if (!alert) {
     return (
       <Snackbar
+        action={action}
         open={open}
         autoHideDuration={duration}
         onClose={onClose}
         message={message}
-        action={action}
+        anchorOrigin={position}
+        sx={sx}
       />
     );
   } else {
     return (
       <Snackbar open={open} autoHideDuration={duration} onClose={onClose}>
-        <Alert severity={"success"} sx={{ width: "100%" }}>
+        <Alert severity={severity} sx={{ width: "100%" }}>
           {message}
         </Alert>
       </Snackbar>
