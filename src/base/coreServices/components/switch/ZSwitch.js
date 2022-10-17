1f625020882ae40f906818aa2fdcc444a2357f9d
h-assefi <assefi.hossein@gmail.com>
2022-10-17 09:59:16 +0330

    ZSwitch (android, ios, ant and default design) #component added

---
+++ src/base/coreServices/components/switch/ZSwitch.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import Switch from "@mui/material/Switch";
+
+import { FormControlLabel, FormGroup } from "@mui/material";
+import IOSSwitch from "./iOSSwitch";
+import Android12Switch from "./Android12Switch";
+import MaterialUISwitch from "./MaterialUISwitch";
+import AntSwitch from "./AntSwitch";
+
+export const ZSwitchColor = {
+  error: "error",
+  success: "success",
+  warning: "warning",
+  info: "info",
+  primary: "primary",
+  secondary: "secondary",
+  default: "default",
+};
+
+export const ZSwitchTheme = {
+  iOS: "iOS",
+  android12: "android12",
+  mui: "mui",
+  ant: "ant",
+  default: "default",
+};
+
+export const ZSwitchLabelPlacement = {
+  top: "top",
+  bottom: "bottom",
+  start: "start",
+  end: "end",
+};
+
+const ZSwitch = ({
+  color = ZSwitchColor.primary,
+  checked,
+  label,
+  labelPlacement = ZSwitchLabelPlacement.end,
+  theme = ZSwitchTheme.default,
+}) => {
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={
+          theme === ZSwitchTheme.iOS ? (
+            <IOSSwitch sx={{ m: 1 }} checked={checked} />
+          ) : theme === ZSwitchTheme.android12 ? (
+            <Android12Switch sx={{ m: 1 }} checked={checked} />
+          ) : theme === ZSwitchTheme.mui ? (
+            <MaterialUISwitch sx={{ m: 1 }} checked={checked} />
+          ) : theme === ZSwitchTheme.ant ? (
+            <AntSwitch sx={{ m: 1 }} checked={checked} />
+          ) : (
+            <Switch color={color} checked={checked} />
+          )
+        }
+        label={label}
+        labelPlacement={labelPlacement}
+      />
+    </FormGroup>
+  );
+};
+
+export default ZSwitch;
