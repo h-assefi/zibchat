6ad6489841fb664e2ef051311ee4646e0fcce6fb
h-assefi <assefi.hossein@gmail.com>
2022-10-19 16:41:54 +0330

    ZButton #component add dir and values

--- src/base/coreServices/components/button/ZButton.js
+++ src/base/coreServices/components/button/ZButton.js
@@ -1,9 +1,28 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
 
+export const ZButtonType = {
+  submit: "submit",
+  reset: "reset",
+  button: "button",
+};
+
+export const ZButtonVariant = {
+  text: "text",
+  outlined: "outlined",
+  contained: "contained",
+};
+
+export const ZButtonSize = {
+  small: "small",
+  medium: "medium",
+  large: "large",
+};
+
 const ZButton = ({
   children,
   color,
+  dir = "rtl",
   disabled,
   href,
   onClick,
@@ -11,17 +30,21 @@ const ZButton = ({
   variant,
   startIcon,
   endIcon,
+  type,
 }) => {
   return (
     <Button
       onClick={onClick}
       variant={variant}
       color={color}
+      dir={dir}
       disabled={disabled}
       href={href}
       size={size}
       startIcon={startIcon}
       endIcon={endIcon}
+      type={type}
+      style={{ fontSize: 16, textTransform: "none" }}
     >
       {children}
     </Button>
