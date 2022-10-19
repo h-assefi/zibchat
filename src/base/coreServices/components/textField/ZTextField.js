2d09ceb928f8f909c6d370d8dcc17cb61d6ec453
h-assefi <assefi.hossein@gmail.com>
2022-10-19 16:43:27 +0330

    ZTextField #component props update and ref

--- src/base/coreServices/components/textField/ZTextField.js
+++ src/base/coreServices/components/textField/ZTextField.js
@@ -13,6 +13,7 @@ export const ZTextFieldType = {
   search: "search",
   number: "number",
   password: "password",
+  text: "text",
 };
 
 export const ZTextFieldSize = {
@@ -29,59 +30,66 @@ export const ZTextFieldColor = {
   secondary: "secondary",
 };
 
-const ZTextField = ({
-  color = ZTextFieldColor.primary,
-  defaultValue,
-  disabled,
-  endIcon,
-  error,
-  fullWidth,
-  helperText,
-  id,
-  label,
-  maxRows,
-  multiline,
-  onChange,
-  placeholder,
-  readOnly = false,
-  required = false,
-  size = ZTextFieldSize.medium,
-  startIcon,
-  type,
-  value,
-
-  variant = ZTextFieldVariant.standard,
-}) => {
-  return (
-    <TextField
-      color={color}
-      defaultValue={defaultValue}
-      disabled={disabled}
-      error={error}
-      fullWidth={fullWidth}
-      helperText={helperText}
-      id={id}
-      InputProps={{
-        readOnly: readOnly,
-        startAdornment: startIcon && (
-          <InputAdornment position="start">{startIcon}</InputAdornment>
-        ),
-        endAdornment: endIcon && (
-          <InputAdornment position="end">{endIcon}</InputAdornment>
-        ),
-      }}
-      label={label}
-      maxRows={maxRows}
-      multiline={multiline}
-      onChange={onChange}
-      placeholder={placeholder}
-      required={required}
-      type={type}
-      value={value}
-      variant={variant}
-      size={size}
-    ></TextField>
-  );
-};
+const ZTextField = React.forwardRef(
+  (
+    {
+      color = ZTextFieldColor.primary,
+      defaultValue,
+      disabled,
+      endIcon,
+      error,
+      fullWidth,
+      helperText,
+      id,
+      label,
+      maxRows,
+      multiline,
+      onChange,
+      placeholder,
+      readOnly = false,
+      required = false,
+      size = ZTextFieldSize.medium,
+      startIcon,
+      type,
+      value,
+      variant = ZTextFieldVariant.standard,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <TextField
+        {...rest}
+        color={color}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        error={error}
+        fullWidth={fullWidth}
+        helperText={helperText}
+        id={id}
+        InputProps={{
+          readOnly: readOnly,
+          startAdornment: startIcon && (
+            <InputAdornment position="start">{startIcon}</InputAdornment>
+          ),
+          endAdornment: endIcon && (
+            <InputAdornment position="end">{endIcon}</InputAdornment>
+          ),
+        }}
+        ref={ref}
+        label={label}
+        maxRows={maxRows}
+        multiline={multiline}
+        onChange={onChange}
+        placeholder={placeholder}
+        required={required}
+        type={type}
+        value={value}
+        variant={variant}
+        size={size}
+      ></TextField>
+    );
+  }
+);
 
 export default ZTextField;
