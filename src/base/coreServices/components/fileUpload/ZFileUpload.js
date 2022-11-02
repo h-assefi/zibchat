c2f44cef3e9158ba70a47865d458ec8d6fecef42
h-assefi <assefi.hossein@gmail.com>
2022-11-02 16:43:03 +0330

    ZFileUpload #component update

--- src/base/coreServices/components/fileUpload/ZFileUpload.js
+++ src/base/coreServices/components/fileUpload/ZFileUpload.js
@@ -1,5 +1,16 @@
-const ZFileUpload = () => {
-  return <input hidden accept="image/*" multiple type="file" />;
+export const ZFileUploadFormat = {
+  all: "*/*",
+  image: "image/*",
+};
+const ZFileUpload = ({ format }) => {
+  return (
+    <input
+      hidden
+      accept={format ?? ZFileUploadFormat.all}
+      multiple
+      type="file"
+    />
+  );
 };
 
 export default ZFileUpload;
